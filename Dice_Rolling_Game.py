5cee9bff99ec0d9d738cc460acee106beabdd87f
Samiul Hasan <106056497+samiul-hasan21@users.noreply.github.com>
2025-09-11 20:02:28 +0600

    Dice_Rolling_Game.py

--- Dice_Rolling_Game.py
+++ Dice_Rolling_Game.py
@@ -1,9 +1,11 @@
 import random
 
+count=0
 history = []
 while True:
-    choice = input('Roll the dice by pressing Enter (y/n)?').lower()
+    choice = input('Roll the dice by pressing Enter (y/n)?').strip().lower()
     print()
+    count+=1
     if choice=='y':
         dice1 = random.randint(1,6)
         dice2 = random.randint(1,6)
@@ -13,12 +15,12 @@ while True:
         else:
             print(f'You rolled {dice1} and {dice2}')  
     elif choice=='n':
-        print('Goodbye')
+        historywant = input('Do you want to see your roll history (y/n)?').strip().lower()   
         break
     else:
         print('Invalid input. Please enter y or n.')
 
-historywant = input('Do you want to see your roll history (y/n)?').lower()
+
 if historywant == 'y':
     print('Your roll history:')
     for i, (d1, d2) in enumerate(history, start=1):
@@ -26,4 +28,13 @@ if historywant == 'y':
             print(f'Roll {i}: Double {d1}')
         else:
             print(f'Roll {i}: {d1} and {d2}')
-
+elif historywant == 'n':
+    print('Then no history for you!')
+else:
+    print('Invalid input. Please enter y or n.')
+print()
+countwant = input('Do you want to see how many times you rolled the dice (y/n)?').strip().lower()
+if countwant == 'y':
+    print(f'You rolled the dice {count} times.')
+elif countwant == 'n':
+    print('Goodbye')
